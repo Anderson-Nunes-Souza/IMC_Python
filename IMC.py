94e3e47e7ead9da9f8c21fd0c985a59aa17620f7
Anderson-Nunes-Souza <anderson.nunes.souza@hotmail.com>
2023-05-13 02:49:39 -0300

    - Dicionário com cenários  adicionado  - Dicionário adicionado com todos os cases e teste iniciando automaticamente ao rodar o arquivo

--- IMC.py
+++ IMC.py
@@ -1,10 +1,4 @@
-'''print("==================================")
-print("App Calculo de IMC")
-print("insira seu peso:")
-peso =  float(input())
-print("insira sua altura:")
-altura = float(input())
-'''
+#Checkagem de Cenarios possíveis
 scenariosdict = {
     "Magreza" : {
         "Altura": 1.70,
@@ -16,57 +10,62 @@ scenariosdict = {
     },
     "Sobrepeso": {
         "Altura": 1.70,
-        "Peso": 90
+        "Peso": 80
     },
     "Obesidade": {
         "Altura": 1.70,
         "Peso": 110
     }
 }
-
-#print(scenariosdict["Magreza"]["Altura"])
+print(type(scenariosdict))
 
 def CalculaIMC(scenario,altura, peso):
     altura = float(scenariosdict[scenario]["Altura"])
     peso = float(scenariosdict[scenario]["Peso"])
+    #IMC = Peso / Altura²
     imc = float(peso / (altura * altura))
     imc_formatado = float("{:.2f}".format(imc))
-    print(imc_formatado)
-'''    if imc_formatado < 18:
-        print("Imc de: " + imc_formatado)
-        print("Magreza")
-    if imc_formatado > 18.5 and imc_formatado < 24.9:
-        print("Imc de: " + imc_formatado)
-        print("Normal")
-    print(imc_formatado)'''
+    
+    #Magreza, quando o resultado é menor que 18,5 kg/m2;
+    if imc_formatado < 18.5:
+            print("Imc de: ")
+            print(imc_formatado)
+            print("Magreza")
+    # Normal, quando o resultado está entre 18,5 e 24,9 kg/m2;
+    if imc_formatado >=18.5 and imc_formatado <=24.9:
+            print("Imc de: ")
+            print(imc_formatado)
+            print("Normal")
+    # Sobrepeso, quando o resultado está entre 24,9 e 30 kg/m2;
+    if imc_formatado >24.9 and imc_formatado <= 30:
+            print("Imc de: ")
+            print(imc_formatado)
+            print("Sobrepeso")
+    # Obesidade, quando o resultado é maior que 30 kg/m2;
+    if imc_formatado >30:
+            print("Imc de: ")
+            print(imc_formatado)
+            print("Obesidade")
 
 
-
+#Função de checkagem de scenarios possíveis
 def SanityCheck():
     for scenario in scenariosdict:
         CalculaIMC(scenario, scenariosdict[scenario]["Altura"], scenariosdict[scenario]["Peso"])
 
+def main():
+    print("==================================")
+    print("App Calculo de IMC")
+    print("insira seu peso:")
+    peso =  float(input())
+    print("insira sua altura:")
+    altura = float(input())
+    #Continuar daqui 
+    scenariosdict["Input_usuario"]["Altura"] = altura
+    scenariosdict["Input_usuario"]["Peso"] = peso 
+    #CalculaIMC(scenariosdict[scenario]["Altura"], scenariosdict[scenario]["Peso"])
+    print(scenariosdict)
+
+
 SanityCheck()
-
-
-'''imc = scenariosdict["Peso"] / (scenariosdict["Altura"] * scenariosdict["Altura"])
-
-imc_formatado = "{:.2f}".format(imc)
-
-print(imc_formatado)'''
-
-
-
-'''
-Faça o cálculo de IMC conforme descrito abaixo (entregue no formato que lhe convier ... web, console, etc)
-
-    Vamos construir um projeto simples, capaz de calcular o IMC (índice de massa corporal), de acordo com a seguinte especificação:
-    IMC = Peso / Altura²
-
-O resultado deve ser expresso, conforme os valores abaixo:
-Magreza, quando o resultado é menor que 18,5 kg/m2;
-Normal, quando o resultado está entre 18,5 e 24,9 kg/m2;
-Sobrepeso, quando o resultado está entre 24,9 e 30 kg/m2;
-Obesidade, quando o resultado é maior que 30 kg/m2;
-Pode ser feita a entrega através do envio de arquivo zip contendo o projeto ou link do github.
-'''
+main()
